0262d13c2e564808fbf02590626536f79557de54
Iskandar Umidov <iskander.umidov.iu@gmail.com>
2022-11-12 15:29:45 -0600

    ports

--- new/starter.c
+++ new/starter.c
@@ -29,32 +29,31 @@ int main(int argc, char *argv[])
     {
         srand(time(NULL));
         int phil_id = get_random_in_range(1, 1000);
-        // pid = fork();
-        // if (pid < 0)
-        // {
-        //     perror("Starter: Fork failed");
-        //     exit(EXIT_FAILURE);
-        // }
-        // else if (pid == 0)
-        // {
-        char phil_id_char[BUFFER_LEN];
-        sprintf(phil_id_char, "%d", phil_id);
-        char read_port_char[BUFFER_LEN];
-        sprintf(read_port_char, "%d", read_ports[i]);
-        char write_port_char[BUFFER_LEN];
-        sprintf(write_port_char, "%d", write_ports[i]);
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("Starter: Fork failed");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            char phil_id_char[BUFFER_LEN];
+            sprintf(phil_id_char, "%d", phil_id);
+            char read_port_char[BUFFER_LEN];
+            sprintf(read_port_char, "%d", read_ports[i]);
+            char write_port_char[BUFFER_LEN];
+            sprintf(write_port_char, "%d", write_ports[i]);
 
-        int next = ((i == PHILOSOPHER_COUNT) ? 0 : (i + 1));
-        char next_read_port_char[BUFFER_LEN];
-        sprintf(next_read_port_char, "%d", read_ports[next]);
-        char next_write_port_char[BUFFER_LEN];
-        sprintf(next_write_port_char, "%d", write_ports[next]);
-        print_log("Exec: Phil ID: %d, Read port: %d, Write port: %d, Next read port: %d, Next write port: %d\n", phil_id, read_ports[i], write_ports[i], read_ports[next], write_ports[next]);
-        // err = execl("./exAppend", "exAppend", phil_id_char, read_port_char, write_port_char, next_read_port_char, next_write_port_char, (char *)NULL); // TODO - change program name, err handle
-        // execl("./exAppend", "exAppend", "PHIL_ID", "SELF_READ_PORT", "SELF_WRITE_PORT", "NEXT_READ_PORT", "NEXT_WRITE_PORT", (char *)NULL);
-        // }
+            int next = ((i == PHILOSOPHER_COUNT) ? 0 : (i + 1));
+            char next_read_port_char[BUFFER_LEN];
+            sprintf(next_read_port_char, "%d", read_ports[next]);
+            char next_write_port_char[BUFFER_LEN];
+            sprintf(next_write_port_char, "%d", write_ports[next]);
+            print_log("Exec: Phil ID: %d, Read port: %d, Write port: %d, Next read port: %d, Next write port: %d\n", phil_id, read_ports[i], write_ports[i], read_ports[next], write_ports[next]);
+            err = execl("./exAppend", "exAppend", phil_id_char, read_port_char, write_port_char, next_read_port_char, next_write_port_char, (char *)NULL); // TODO - change program name, err handle
+            // execl("./exAppend", "exAppend", "PHIL_ID", "SELF_READ_PORT", "SELF_WRITE_PORT", "NEXT_READ_PORT", "NEXT_WRITE_PORT", (char *)NULL);
+        }
         sleep(1);
-        // usleep(1000);
     }
 
     // for (;;)
